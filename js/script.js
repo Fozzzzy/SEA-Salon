26d2847a41b1068dafe0bee3ea74873af27e5eb2
Fozzzzy <122066488+Fozzzzy@users.noreply.github.com>
2024-06-27 02:10:57 +0700

    Added hamburger menu and improved responsiveness

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// HAMBURGER MENU
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll("li").forEach(n => n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+}));
+
+
+
+// NAVBAR SHADOW
+document.addEventListener('DOMContentLoaded', () => {
+    var headers = document.querySelectorAll('header');
+
+    window.addEventListener('scroll', function () {
+        headers.forEach(function (header) {
+            if (window.scrollY > 0) {
+                header.classList.add('shadow');
+            } else {
+                header.classList.remove('shadow');
+            }
+        });
+    });
+});
